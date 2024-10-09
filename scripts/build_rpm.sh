bfb9377c94a0e377b2c6d709b69d867804b42feb
kovarike <danilokovarike@gmail.com>
2024-10-09 12:37:13 -0300

    lib

--- scripts/build_rpm.sh
+++ scripts/build_rpm.sh
@@ -54,7 +54,7 @@ set -e
 
 VERSION="1.0.0"
 PACKAGE_NAME="uuid"
-RPMBUILD_DIR="/tmp/rpmbuild"
+RPMBUILD_DIR="/rpmbuild"
 
 # Cria as pastas necessárias para o rpmbuild
 mkdir -p $RPMBUILD_DIR/SOURCES
@@ -64,9 +64,14 @@ mkdir -p $RPMBUILD_DIR/SPECS  # Certifique-se de criar o diretório SPECS
 cmake -S . -B build
 cmake --build build
 
-cd /home/runner/work/uuid-for-c
+# cd /home/runner/work/uuid-for-c
 # Gera o arquivo tar.gz da fonte
-tar czvf $RPMBUILD_DIR/SOURCES/$PACKAGE_NAME-$VERSION.tar.gz . --exclude-vcs --transform="s|^|$PACKAGE_NAME-$VERSION/|" .
+tar czvf $RPMBUILD_DIR/SOURCES/$PACKAGE_NAME-$VERSION.tar.gz .
+
+if [[ ! -f $RPMBUILD_DIR/SOURCES/$PACKAGE_NAME-$VERSION.tar.gz ]]; then
+    echo "Erro: O arquivo $PACKAGE_NAME-$VERSION.tar.gz não foi criado."
+    exit 1
+fi
 
 # Gera o .spec dinâmico
 cat <<EOF > $RPMBUILD_DIR/SPECS/$PACKAGE_NAME.spec
