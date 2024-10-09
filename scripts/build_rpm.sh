38725d70dc103386b9a1de1f639556be2b688e22
kovarike <danilokovarike@gmail.com>
2024-10-09 12:11:38 -0300

    lib

--- scripts/build_rpm.sh
+++ scripts/build_rpm.sh
@@ -1,3 +1,54 @@
+# #!/bin/bash
+# set -e
+
+# VERSION="1.0.0"
+# PACKAGE_NAME="uuid"
+# RPMBUILD_DIR="/tmp/rpmbuild"
+
+# # Cria as pastas necessárias para o rpmbuild
+# mkdir -p $RPMBUILD_DIR/SOURCES
+# mkdir -p $RPMBUILD_DIR/SPECS
+
+# # Compila o código
+# cmake -S . -B build
+# cmake --build build
+
+# # Gera o arquivo tar.gz da fonte
+# tar czvf $RPMBUILD_DIR/SOURCES/$PACKAGE_NAME-$VERSION.tar.gz .
+
+# # Gera o .spec dinâmico
+# cat <<EOF > $RPMBUILD_DIR/SPECS/$PACKAGE_NAME.spec
+# Name:           $PACKAGE_NAME
+# Version:        $VERSION
+# Release:        1%{?dist}
+# Summary:        Biblioteca de uuid para C.
+
+# License:        MIT
+# Source0:        %{name}-%{version}.tar.gz
+# BuildRequires:  cmake, gcc
+# Requires:       libc.so.6
+
+# %description
+# Biblioteca de uuid para C.
+
+# %prep
+# %setup -q
+
+# %build
+# cmake .
+# make
+
+# %install
+# make install DESTDIR=%{buildroot}
+
+# %files
+# /usr/lib/libuuid.so
+# /usr/include/uuid.h
+# EOF
+
+# # Compilar o pacote RPM
+# rpmbuild -ba $RPMBUILD_DIR/SPECS/$PACKAGE_NAME.spec
+
 #!/bin/bash
 set -e
 
@@ -7,13 +58,14 @@ RPMBUILD_DIR="/tmp/rpmbuild"
 
 # Cria as pastas necessárias para o rpmbuild
 mkdir -p $RPMBUILD_DIR/SOURCES
+mkdir -p $RPMBUILD_DIR/SPECS  # Certifique-se de criar o diretório SPECS
 
 # Compila o código
 cmake -S . -B build
 cmake --build build
 
 # Gera o arquivo tar.gz da fonte
-tar czvf $RPMBUILD_DIR/SOURCES/$PACKAGE_NAME-$VERSION.tar.gz .
+tar czvf $RPMBUILD_DIR/SOURCES/$PACKAGE_NAME-$VERSION.tar.gz . 
 
 # Gera o .spec dinâmico
 cat <<EOF > $RPMBUILD_DIR/SPECS/$PACKAGE_NAME.spec
@@ -34,7 +86,7 @@ Biblioteca de uuid para C.
 %setup -q
 
 %build
-cmake .
+cmake . 
 make
 
 %install
